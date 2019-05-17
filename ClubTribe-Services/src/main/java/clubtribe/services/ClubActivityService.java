5822199048d99878e894e9c6dd93c3c770b4f918
MQ <870830369@qq.com>
2019-05-17 16:16:34 +0800

    MQ

--- ClubTribe-Services/src/main/java/clubtribe/services/ClubActivityService.java
+++
@@ -1,9 +0,0 @@
-package clubtribe.services;
-
-import java.util.List;
-
-public interface ClubActivityService {
-    String queryClubidsByUserid(String userid);
-
-    List<String> queryClubsByClubid(String clubid);
-}
