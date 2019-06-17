bd61f48331fc1e92a83dd21a766818feab8a205f
QGshen <870830369@qq.com>
2019-06-17 16:52:10 +0800

    MQ

---
+++ ClubTribe-Dao/src/main/java/clubtribe/dao/ActivityMapper.java
@@ -0,0 +1,35 @@
+package clubtribe.dao;
+
+import clubtribe.pojo.Activity;
+
+import java.util.ArrayList;
+
+/**
+ * @author QGshen
+ */
+public interface ActivityMapper {
+    ArrayList<Integer> getids();
+
+    /**
+     * 插入新活动
+     *
+     * @param activity
+     * @return
+     */
+    Integer insertNewActivity(Activity activity);
+
+    /**
+     * 获取所有活动
+     *
+     * @return
+     */
+    ArrayList<Activity> getallactivity();
+
+    /**
+     * 获取活动byid
+     *
+     * @return
+     */
+    ArrayList<Activity> getactivitbyid(String clubid);
+
+}
